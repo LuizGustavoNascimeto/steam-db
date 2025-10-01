8dab3892c93a63df698d2fedc1e9b658035a5af9
LuizGustavoNascimeto <luizgnf1@gmail.com>
2025-10-01 17:28:54 -0300

    mudando para rawg

--- go.mod
+++ go.mod
@@ -3,13 +3,11 @@ module steam-db
 go 1.25.1
 
 require (
-	github.com/PuerkitoBio/goquery v1.10.3
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.0
 )
 
 require (
-	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
@@ -17,7 +15,6 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/net v0.39.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 )
--- types/types.go
+++ types/types.go
@@ -1,99 +1,81 @@
 package types
 
-type SteamResponse struct {
-	Success bool           `json:"success"`
-	Data    GameDetailsRes `json:"data"`
-}
+import "database/sql"
 
+type RawgResponse struct {
+	Count int       `json:"count"`
+	Next  string    `json:"next"`
+	Prev  string    `json:"previous"`
+	Res   []GameRes `json:"results"`
+}
+type RawgResponse2 struct {
+	Count int     `json:"count"`
+	Next  string  `json:"next"`
+	Prev  string  `json:"previous"`
+	Res   []Store `json:"results"`
+}
 type Game struct {
-	Appid int    `json:"appid" gorm:"primaryKey"`
-	Name  string `json:"name"`
+	ID           int            `json:"id" gorm:"primaryKey;column:id"`
+	Name         string         `json:"name" gorm:"column:name"`
+	Released     sql.NullString `json:"released" gorm:"column:released;type:date"`
+	Rating       float64        `json:"rating" gorm:"column:rating"`
+	RatingsCount int            `json:"ratings_count" gorm:"column:ratings_count"`
+	Metacritic   int            `json:"metacritic" gorm:"column:metacritic;type:int"`
+	Owned        int            `json:"owned" gorm:"column:owned;type:int"`
+	Beaten       int            `json:"beaten" gorm:"column:beaten;type:int"`
+	Toplay       int            `json:"toplay" gorm:"column:toplay;type:int"`
+	Dropped      int            `json:"dropped" gorm:"column:dropped;type:int"`
+	Playing      int            `json:"playing" gorm:"column:playing;type:int"`
+	Yet          int            `json:"yet" gorm:"column:yet;type:int"`
+	Platforms    []Platform     `json:"platforms" gorm:"many2many:game_platforms;"`
+	Stores       []Store        `json:"stores" gorm:"many2many:game_stores;"`
+	Genres       []Genre        `json:"genres" gorm:"many2many:game_genres;"`
+	Tags         []Tag          `json:"tags" gorm:"many2many:game_tags;"`
 }
-type GameDetails struct {
-	Appid             int        `json:"steam_appid" gorm:"primaryKey"`
-	Name              string     `json:"name" gorm:"column:name"`
-	Type              string     `json:"type" gorm:"column:type"`
-	Is_free           bool       `json:"is_free" gorm:"column:is_free"`
-	Short_description string     `json:"short_description" gorm:"column:short_description"`
-	Developers        string     `json:"developers" gorm:"column:developers"`
-	Currency          string     `json:"currency" gorm:"column:currency"`
-	Price             int        `json:"price" gorm:"column:price"`
-	Mac               bool       `json:"mac" gorm:"column:mac"`
-	Windows           bool       `json:"windows" gorm:"column:windows"`
-	Linux             bool       `json:"linux" gorm:"column:linux"`
-	Metacritic        int        `json:"metacritic" gorm:"column:metacritic"`
-	Recommendations   int        `json:"recommendations" gorm:"column:recommendations"`
-	Release_at        string     `json:"release_date" gorm:"column:release_at;type:date"`
-	Genres            []Genre    `gorm:"many2many:game_has_genres;joinForeignKey:AppID;joinReferences:GenreID"`
-	Categories        []Category `gorm:"many2many:game_has_categories;joinForeignKey:AppID;joinReferences:CategoryID"`
-}
-type GameDetailsRes struct {
-	Appid             int           `json:"steam_appid" gorm:"primaryKey"`
-	Name              string        `json:"name"`
-	Type              string        `json:"type"`
-	Is_free           bool          `json:"is_free"`
-	Short_description string        `json:"short_description"`
-	Developers        []string      `json:"developers"`
-	Price_overview    PriceOverview `json:"price_overview"`
-	Platforms         Plataform     `json:"platforms"`
-	Metacritic        Metacritic    `json:"metacritic"`
-	Categories        []CategoryRes `json:"categories" gorm:"-"`
-	Genres            []GenreRes    `json:"genres" gorm:"-"`
-	Recommended       Recommended   `json:"recommendations"`
-	Release_date      ReleaseDate   `json:"release_date"`
-}
-
-type Category struct {
-	ID          int    `json:"id" gorm:"primaryKey"`
-	Description string `json:"description"`
+type Tag struct {
+	ID   int    `json:"id" gorm:"primaryKey;column:id"`
+	Name string `json:"name" gorm:"uniqueIndex;column:name"`
 }
 type Genre struct {
-	ID          int    `json:"id" gorm:"primaryKey"`
-	Description string `json:"description"`
+	ID   int    `json:"id" gorm:"primaryKey;column:id"`
+	Name string `json:"name" gorm:"uniqueIndex;column:name"`
 }
-type GameHasGenre struct {
-	AppID   int `gorm:"column:app_id;primaryKey"`
-	GenreID int `gorm:"column:genre_id;primaryKey"`
-
-	Game  GameDetails `gorm:"foreignKey:AppID;references:Appid;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Genre Genre       `gorm:"foreignKey:GenreID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+type Platform struct {
+	ID   int    `json:"id" gorm:"primaryKey;column:id"`
+	Name string `json:"name" gorm:"uniqueIndex;column:name"`
 }
-
-type GameHasCategory struct {
-	AppID      int `gorm:"column:app_id;primaryKey"`
-	CategoryID int `gorm:"column:category_id;primaryKey"`
-
-	Game     GameDetails `gorm:"foreignKey:AppID;references:Appid;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Category Category    `gorm:"foreignKey:CategoryID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+type Store struct {
+	ID   int    `json:"id" gorm:"primaryKey;column:id"`
+	Name string `json:"name" gorm:"uniqueIndex;column:name"`
 }
-
-type PriceOverview struct {
-	Currency string `json:"currency"`
-	Initial  int    `json:"initial"`
+type Developer struct {
+	ID   int    `json:"id" gorm:"primaryKey;column:id"`
+	Name string `json:"name" gorm:"uniqueIndex;column:name"`
 }
-
-type Plataform struct {
-	Windows bool `json:"windows"`
-	Mac     bool `json:"mac"`
-	Linux   bool `json:"linux"`
+type GameRes struct {
+	ID            int            `json:"id"`
+	Name          string         `json:"name"`
+	Released      string         `json:"released"`
+	Rating        float64        `json:"rating"`
+	RatingsCount  int            `json:"ratings_count"`
+	Metacritic    int            `json:"metacritic"`
+	AddedByStatus AddStatus      `json:"added_by_status"`
+	Platforms     []PlatformsRes `json:"platforms"`
+	Stores        []StoreRes     `json:"stores"`
+	Genres        []Genre        `json:"genres"`
+	Tags          []Tag          `json:"tags"`
 }
-
-type Metacritic struct {
-	Score int    `json:"score"`
-	URL   string `json:"url"`
+type AddStatus struct {
+	Owned   int `json:"owned"`
+	Beaten  int `json:"beaten"`
+	Toplay  int `json:"toplay"`
+	Dropped int `json:"dropped"`
+	Playing int `json:"playing"`
+	Yet     int `json:"yet"`
 }
-type CategoryRes struct {
-	ID          int    `json:"id"`
-	Description string `json:"description"`
+type PlatformsRes struct {
+	Platform Platform `json:"platform"`
 }
-type GenreRes struct {
-	ID          string `json:"id"`
-	Description string `json:"description"`
-}
-type Recommended struct {
-	Total int `json:"total"`
-}
-type ReleaseDate struct {
-	ComingSoon bool   `json:"coming_soon"`
-	Date       string `json:"date"`
+type StoreRes struct {
+	Store Store `json:"store"`
 }
